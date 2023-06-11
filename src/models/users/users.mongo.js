6d4611ef1079736c10e5b43c96b49e5313b51c20
RummahH <jah.hasant@gmail.com>
2023-06-11 23:06:07 +0600

    added schema fields

--- src/models/users/users.mongo.js
+++ src/models/users/users.mongo.js
@@ -3,24 +3,31 @@ const crypto = require('crypto');
 const { Schema, model } = require('mongoose');
 const validator = require('validator');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, 'Please tell us your name!']
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please tell us your name!']
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email!'],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, 'Please provide a valid email address!']
+    },
+    email_verified: { type: Boolean, default: false },
+    last_login: {
+      type: Date,
+      default: Date.now()
+    }
   },
-  email: {
-    type: String,
-    required: [true, 'Please provide your email!'],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email address!']
-  },
-  email_verified: { type: Boolean, default: false },
-  last_login: {
-    type: Date,
-    default: Date.now()
+  {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
   }
-});
+);
 
 const User = model('User', userSchema);
 module.exports = User;
